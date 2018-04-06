8261b5dd47e578ac64c4804b82af7173ccda5586
mc-b <marcel.bernet@ch-open.ch>
2018-04-06 11:46:14 +0200

    dpkg hat kein -y Argument

--- installvv.sh
+++ installvv.sh
@@ -18,7 +18,7 @@ sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.4-119785
 # Vagrant
 wget https://releases.hashicorp.com/vagrant/2.0.3/vagrant_2.0.3_x86_64.deb?_ga=2.21104755.952897466.1521814486-1390085671.1521814486
 mv vagrant_2.0.3_x86_64.deb\?_ga\=2.21104755.952897466.1521814486-1390085671.1521814486  vagrant_2.0.3_x86_64.deb
-sudo dpkg -i -y vagrant_2.0.3_x86_64.deb
+sudo dpkg -i vagrant_2.0.3_x86_64.deb
 vagrant plugin install vagrant-disksize
 
 
