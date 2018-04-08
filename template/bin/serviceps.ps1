52981659fc5345bf225cd58b72682116123bc258
mc-b <marcel.bernet@ch-open.ch>
2018-04-08 11:34:27 +0200

    ohne /fhem

--- template/bin/serviceps.ps1
+++ template/bin/serviceps.ps1
@@ -2,4 +2,4 @@ $service=$args[0]
 
 start (kubectl config view -o=jsonpath='{ .clusters[0].cluster.server }' | `
 %{ $_-replace "https:","http:"} | `
-%{ $_-replace "6443", (kubectl get svc $service -o=jsonpath='{ .spec.ports[0].nodePort }')})/fhem
+%{ $_-replace "6443", (kubectl get svc $service -o=jsonpath='{ .spec.ports[0].nodePort }')})
