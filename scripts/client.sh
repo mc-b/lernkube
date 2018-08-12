28ffe7701715668086a9f997c7264f300bf92fb5
mc-b <marcel.bernet@ch-open.ch>
2018-08-12 14:19:52 +0200

    Ausgabe Infos zur Umgebung

--- scripts/client.sh
+++ scripts/client.sh
@@ -2,6 +2,30 @@
 #
 # 	Aufbereitung Client Scripts und Programme.
 #
+
+function info
+{
+cat <<%EOF%
+echo "===================================================================="
+echo "VM: $(hostname), Cluster-IP: $(hostname -I | cut -d ' ' -f 2)"
+echo ""
+echo "dashboard - Aufruf Dashboard"
+echo "weave - Aufruf Weave ein Werkzeug zur grafischen Visualisierung der Container"
+echo ""
+echo "kubectl create -f YAML-Datei - Service, laut YAML-Datei, starten"
+echo "kubectl delete -f YAML-Datei - Service, laut YAML-Datei, loeschen"
+echo "startsvc 'service' - Oeffnet Service UI im Browser"
+echo "runbash 'service' - Wechselt in die Bash des Containers 'service'"
+echo ""
+echo "docker image ls - Anzeige der Images"
+echo "docker build - Builden eines Images laut Dockerfile"
+echo ""
+echo "vagrant ssh $(hostname) - Wechselt in die VM"
+echo "===================================================================="
+%EOF%
+}
+
+
 sudo apt-get install -q 2 -y dos2unix bsdtar
 
 # wenn nur 1 Master Scripts in /vagrant ablegen
@@ -20,11 +44,12 @@ fi
 # Docker - Externer Zugriff
 sudo cp -rp /home/vagrant/.docker $OUT/
 
-# Externer Zugriff
+# kuberntes - Externer Zugriff
 cp -rp $HOME/.kube $OUT/
 
 # Dashboard
 cat >$OUT/dashboard.bat <<%EOF%
+@ECHO OFF
 REM Startet den Browser mit der Dashboard Startseite und den Proxy 
 cd /d %~d0%~p0
 set DOCKER_HOST=tcp://$(hostname -I | cut -d ' ' -f 2):2376
@@ -33,12 +58,13 @@ set DOCKER_CERT_PATH=%~d0%~p0.docker
 set PATH=%PATH%;%~d0%~p0bin
 set KUBECONFIG=%~d0%~p0.kube\\config
 start http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/. /B
-kubectl proxy        
+kubectl proxy     
 %EOF%
 unix2dos $OUT/dashboard.bat
 
 # dockerps.bat	
 cat >$OUT/dockerps.bat <<%EOF%
+@ECHO OFF
 REM Setzt die Docker Umgebungsvariablen und startet PowerShell 
 cd /d %~d0%~p0
 set DOCKER_HOST=tcp://$(hostname -I | cut -d ' ' -f 2):2376
@@ -46,12 +72,14 @@ set DOCKER_TLS_VERIFY=1
 set DOCKER_CERT_PATH=%~d0%~p0.docker
 set PATH=%~d0%~p0bin;~d0%~p0git\\bin;%~d0%~p0git\\mingw64\\bin;%~d0%~p0git\\usr\\bin;%PATH%
 set KUBECONFIG=%~d0%~p0.kube\\config
-powershell.exe      
+$(info)  
+powershell.exe    
 %EOF%
 unix2dos $OUT/dockerps.bat
 
 # dockersh.bat	
 cat >$OUT/dockersh.bat <<%EOF%
+@ECHO OFF
 REM Setzt die Docker Umgebungsvariablen und startet Git/Bash 
 cd /d %~d0%~p0
 set DOCKER_HOST=tcp://$(hostname -I | cut -d ' ' -f 2):2376
@@ -59,6 +87,7 @@ set DOCKER_TLS_VERIFY=1
 set DOCKER_CERT_PATH=%~d0%~p0.docker
 set PATH=%PATH%;%~d0%~p0bin
 set KUBECONFIG=%~d0%~p0.kube\\config
+$(info)  
 start git-bash.exe   
 %EOF%
 unix2dos $OUT/dockersh.bat	
@@ -72,6 +101,7 @@ export DOCKER_TLS_VERIFY=1
 export DOCKER_CERT_PATH=\$(pwd)/.docker
 export PATH=\$PATH:\$(pwd)/bin:\$(pwd)
 export KUBECONFIG=\$(pwd)/.kube/config
+$(info)  
 %EOF%
 
 # kubectl CLI
@@ -80,7 +110,3 @@ curl -s -L https://storage.googleapis.com/kubernetes-release/release/v1.10.0/bin
 ( cd $OUT/bin/ && curl -s -L https://download.docker.com/win/static/stable/x86_64/docker-17.09.0-ce.zip | bsdtar xvf - && mv docker/docker.exe . && rm -rf docker)
 # helm CLI
 ( cd $OUT/bin/ && curl -s -L https://storage.googleapis.com/kubernetes-helm/helm-v2.10.0-rc.3-windows-amd64.zip | bsdtar xvf - && mv windows-amd64/helm.exe . && rm -rf windows-amd64)
-
-
-
-
