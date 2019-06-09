d41f5c8daa74283c300a216ccb5fbb6783d8ae9a
mc-b <marcel.bernet@ch-open.ch>
2019-06-09 21:45:55 +0200

    contains worker

--- scripts/k8sbase.sh
+++ scripts/k8sbase.sh
@@ -12,7 +12,7 @@ sudo apt-get -q 2 update
 sudo apt-get install -q 2 -y kubelet=${VERSION} kubeadm=${VERSION}
 
 # Bug vagrant, @see https://linuxacademy.com/community/posts/show/topic/29447-pod-is-not-found-eventhough-pod-status-is-up-and-running-why
-if  [ "$(hostname | cut -d- -f1)" = "worker" ]
+if  [[ "$(hostname)" =~ "worker" ]]
 then
     cat <<%EOF% >>/etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 Environment="KUBELET_EXTRA_ARGS=--node-ip=$(hostname -I | cut '-d ' -f2)"
