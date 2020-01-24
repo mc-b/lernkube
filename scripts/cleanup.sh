4921f04e69db2bdf100bc1b636ebe94b8410fef4
mc-b <marcel.bernet@ch-open.ch>
2020-01-24 17:19:14 +0100

    Docker Cache

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,4 +4,15 @@
 #
 
 sudo rm -f /home/vagrant/docker*.deb
+
+mkdir -p /vagrant/cr-cache
+
+for i in $(docker images | cut -d ' ' -f 1 | grep -v REPOSITORY)
+do
+    OUT="/vagrant/cr-cache/$(echo ${i} | tr / _).tar"
+    [ ! -f ${OUT} ] && { echo "save image ${i} to ${OUT}"; docker save ${i} -o ${OUT}; }
+done
+
 # umount /vagrant
+
+exit 0
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,10 +2,21 @@
 #
 # 	Installiert Docker bzw. Feintuning
 
-    set -o xtrace
+set -o xtrace
 
-    sudo apt-get install -y libltdl7
-	wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb >/dev/null 2>&1
-    sudo dpkg -i docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb
-    sudo usermod -aG docker vagrant 
+sudo apt-get install -y libltdl7
+wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb >/dev/null 2>&1
+sudo dpkg -i docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb
+sudo usermod -aG docker vagrant 
     
+# Abhandlung Container Cache
+
+if  [ -d /vagrant/cr-cache/ ]
+then
+    for image in /vagrant/cr-cache/*.tar
+    do
+        sudo docker load -i ${image}
+    done
+fi
+
+sudo docker image ls
