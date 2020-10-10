f79d8497eb4c21f6863186ef842f3386220c6bf6
mc-b <marcel.bernet@ch-open.ch>
2020-10-10 17:31:22 +0200

    neuste Version Dashboard

--- scripts/k8saddons.sh
+++ scripts/k8saddons.sh
@@ -4,7 +4,7 @@
 #
 
 # Dashboard und User einrichten - Zugriff via kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}') Ermitteln
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-rc5/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml
 kubectl apply -f /vagrant/addons/dashboard-admin.yaml
 
 # Standard Persistent Volume und Claim
